78642094320295e8dfa5905a40ff0213330fb081
nathbns <104826275+nathbns@users.noreply.github.com>
2025-05-18 09:15:57 +0200

    test deploy3

--- drizzle/db/index.ts
+++ drizzle/db/index.ts
@@ -1,9 +1,21 @@
-import "dotenv/config"
+import "dotenv/config";
 
-import { drizzle } from "drizzle-orm/node-postgres"
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-import * as schema from "./schema"
+import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
-  schema,
-})
+// Vérifier si DATABASE_URL est définie
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL environment variable is not set or accessible."
+  );
+}
+
+// Créer un Pool de connexions
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // DATABASE_URL doit contenir ?sslmode=require
+});
+
+// Initialiser Drizzle avec le pool et le schéma
+export const db = drizzle(pool, { schema });
